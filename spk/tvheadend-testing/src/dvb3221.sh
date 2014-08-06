a1c3b89f0742269cc8fdb102ad0208255d06f250
sangood <sangood@macmini.lan>
2014-08-06 11:23:50 +0800

    Spksrc  tvheadend-test's  cross and spk.
    Tested on dsm 5.0 sdk  gcc463 .

---
+++ spk/tvheadend-testing/src/dvb3221.sh
@@ -0,0 +1,6 @@
+#insmod /usr/local/tvheadend-testing/lib/dvb3221/i2c-core.ko
+insmod /usr/local/tvheadend-testing/lib/dvb3221/dvb-core.ko
+insmod /usr/local/tvheadend-testing/lib/dvb3221/dvb-usb.ko
+insmod /usr/local/tvheadend-testing/lib/dvb3221/dvb-pll.ko
+insmod /usr/local/tvheadend-testing/lib/dvb3221/tda10023.ko
+insmod /usr/local/tvheadend-testing/lib/dvb3221/dvb3221.ko
---
+++ spk/tvheadend/src/dvb3221.sh
@@ -0,0 +1,5 @@
+insmod /usr/local/tvheadend/lib/dvb3221/dvb-core.ko
+insmod /usr/local/tvheadend/lib/dvb3221/dvb-usb.ko
+insmod /usr/local/tvheadend/lib/dvb3221/dvb-pll.ko
+insmod /usr/local/tvheadend/lib/dvb3221/tda10023.ko
+insmod /usr/local/tvheadend/lib/dvb3221/dvb3221.ko
